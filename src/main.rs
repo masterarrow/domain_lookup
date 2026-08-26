ee74a543135b1ca325aa18072aee4069484b68c3
Vitalii Kisielov <knv@fozzy.com>
2026-08-26 18:21:29 +0300

    Rate limit

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use std::sync::Arc;
+use std::{sync::Arc, time::Duration};
 use axum::{
     routing::get,
     Router,
 };
+use std::net::SocketAddr;
+use tower_governor::{governor::GovernorConfigBuilder, GovernorLayer};
 use domain_lookup::{
     AppState,
     Config,
@@ -21,9 +23,28 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let config = Config::new();
     let port = config.port;
     let client = HttpClient::new(config);
+
+    // Rate limit 60 requests per minute with a burst size of 20
+    let governor_conf = Arc::new(
+        GovernorConfigBuilder::default()
+            .per_second(1)
+            .burst_size(20)
+            .finish()
+            .unwrap(),
+    );
+    let governor_limiter = governor_conf.limiter().clone();
+    let interval = Duration::from_secs(60);
+
     let state = AppState {
         client: Arc::new(client),
     };
+
+    std::thread::spawn(move || {
+       loop {
+           std::thread::sleep(interval);
+           governor_limiter.retain_recent();
+       }
+   });
 
     // Axum routes
     let app = Router::new()
@@ -34,12 +55,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         .route("/subdomain", get(subdomain_lookup))
         .route("/ssl-check", get(ssl_lookup))
         .route("/health", get(health_check))
+        .layer(GovernorLayer::new(governor_conf))
         .with_state(state);
 
-    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port)).await?;
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
+    let listener = tokio::net::TcpListener::bind(addr).await?;
     println!("{}", format!("HTTP Server started on  http://0.0.0.0:{}", port));
 
-    axum::serve(listener, app).await?;
+    axum::serve(listener, app.into_make_service_with_connect_info::<SocketAddr>()).await?;
 
     Ok(())
 }
